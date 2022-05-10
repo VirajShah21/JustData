0962dedd20a4e60e09c02a1dfd34600992adf1fc
Viraj Shah <shahvirajd@gmail.com>
2022-05-10 17:32:31 -0400

    Created sub-components for the landing page

--- app/src/LandingPage.tsx
+++ app/src/LandingPage.tsx
@@ -7,46 +7,55 @@ import logo from './logo.png';
 import stocksLogo from './stocks.png';
 import mostWantedLogo from './Just Most Wanted.png';
 
+function LandingLeft() {
+    return (
+        <VStack className='landing-left'>
+            <Spacer />
+            <img src={logo} alt='Just Data' className='landing-brand' />
+            <Spacer />
+            <span className='bg-attribution' style={{ alignSelf: 'start' }}>
+                Photo by Maximalfocus on Unsplash
+            </span>
+        </VStack>
+    );
+}
+
+function LandingRight() {
+    return (
+        <VStack className='landing-right'>
+            <SearchBar placeholder='Search Our Products' />
+
+            <div style={{ height: '2rem' }} />
+
+            <ProductSectionTitle icon={<GraphIcon />} label='Financial Markets' />
+
+            <HStack justify='around'>
+                <ProductResult icon={stocksLogo} label='Stocks' url='stocks' />
+            </HStack>
+
+            {/* Stock information */}
+
+            <ProductSectionTitle icon={<ArchiveIcon />} label='U.S. Intelligence' />
+
+            {/* CIA World Factbook */}
+
+            <ProductSectionTitle icon={<LawIcon />} label='Crime & Justice' />
+
+            <HStack justify='around'>
+                <ProductResult icon={mostWantedLogo} label='FBI Most Wanted' url='most-wanted' />
+            </HStack>
+
+            <Spacer />
+        </VStack>
+    );
+}
+
 function LandingPage() {
     return (
         <HStack height='100%'>
-            <VStack className='landing-left'>
-                <Spacer />
-                <img src={logo} alt='Just Data' className='landing-brand' />
-                <Spacer />
-                <span className='bg-attribution' style={{ alignSelf: 'start' }}>
-                    Photo by Maximalfocus on Unsplash
-                </span>
-            </VStack>
+            <LandingLeft />
             <VStack className='landing-divider'>&nbsp;</VStack>
-            <VStack className='landing-right'>
-                <SearchBar placeholder='Search Our Products' />
-                <div style={{ height: '2rem' }} />
-
-                <ProductSectionTitle icon={<GraphIcon />} label='Financial Markets' />
-
-                <HStack justify='around'>
-                    <ProductResult icon={stocksLogo} label='Stocks' url='stocks' />
-                </HStack>
-
-                {/* Stock information */}
-
-                <ProductSectionTitle icon={<ArchiveIcon />} label='U.S. Intelligence' />
-
-                {/* CIA World Factbook */}
-
-                <ProductSectionTitle icon={<LawIcon />} label='Crime & Justice' />
-
-                <HStack justify='around'>
-                    <ProductResult
-                        icon={mostWantedLogo}
-                        label='FBI Most Wanted'
-                        url='most-wanted'
-                    />
-                </HStack>
-
-                <Spacer />
-            </VStack>
+            <LandingRight />
         </HStack>
     );
 }
