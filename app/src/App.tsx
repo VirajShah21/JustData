c5cf0bf6936319ff3bd2a5384ce675b6faecbda4
Viraj Shah <shahvirajd@gmail.com>
2022-05-10 06:09:19 -0400

    Fixed up a standard landing page

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,7 +1,45 @@
 import './App.css';
+import { HStack, VStack, Spacer } from 'reaction';
+import logo from './logo.png';
+import { ArrowRightIcon, SearchIcon } from '@primer/octicons-react';
+
+function SearchBar(props: { placeholder?: string; value?: string }) {
+    return (
+        <HStack className='search-bar'>
+            <span className='search-icon-wrapper'>
+                <SearchIcon />
+            </span>
+            <input
+                type='text'
+                value={props.value}
+                placeholder={props.placeholder}
+                className='search-bar-input'
+            />
+            <button className='search-button'>
+                <ArrowRightIcon size={25} />
+            </button>
+        </HStack>
+    );
+}
 
 function LandingPage() {
-    return <div>Hello World</div>;
+    return (
+        <HStack height='100%'>
+            <VStack className='landing-left'>
+                <Spacer />
+                <img src={logo} alt='Just Data' className='landing-brand' />
+                <Spacer />
+                <span className='bg-attribution' style={{ alignSelf: 'start' }}>
+                    Photo by Maximalfocus on Unsplash
+                </span>
+            </VStack>
+            <VStack className='landing-divider'>&nbsp;</VStack>
+            <VStack className='landing-right'>
+                <SearchBar placeholder='Search Products and Services' />
+                <Spacer />
+            </VStack>
+        </HStack>
+    );
 }
 
 function App() {
