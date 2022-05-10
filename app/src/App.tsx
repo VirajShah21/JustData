efb275b31cc65c7abc0bc4c006d9364b8553f4e7
Viraj Shah <shahvirajd@gmail.com>
2022-05-10 17:29:37 -0400

    Moved landing page out

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,62 +1,8 @@
-import {
-    ArchiveIcon,
-    ArrowRightIcon,
-    GraphIcon,
-    LawIcon,
-    SearchIcon,
-} from '@primer/octicons-react';
-import { HStack, Spacer, VStack } from 'reaction';
-import './App.css';
-import logo from './logo.png';
-import stocksLogo from './stocks.png';
 import { AdvancedRealTimeChart } from 'react-ts-tradingview-widgets';
-import mostWantedLogo from './Just Most Wanted.png';
-import { ProductSectionTitle, ProductResult } from './components/ProductComponents';
+import { VStack } from 'reaction';
+import './App.css';
 import SearchBar from './components/SearchBar';
-
-function LandingPage() {
-    return (
-        <HStack height='100%'>
-            <VStack className='landing-left'>
-                <Spacer />
-                <img src={logo} alt='Just Data' className='landing-brand' />
-                <Spacer />
-                <span className='bg-attribution' style={{ alignSelf: 'start' }}>
-                    Photo by Maximalfocus on Unsplash
-                </span>
-            </VStack>
-            <VStack className='landing-divider'>&nbsp;</VStack>
-            <VStack className='landing-right'>
-                <SearchBar placeholder='Search Our Products' />
-                <div style={{ height: '2rem' }} />
-
-                <ProductSectionTitle icon={<GraphIcon />} label='Financial Markets' />
-
-                <HStack justify='around'>
-                    <ProductResult icon={stocksLogo} label='Stocks' url='stocks' />
-                </HStack>
-
-                {/* Stock information */}
-
-                <ProductSectionTitle icon={<ArchiveIcon />} label='U.S. Intelligence' />
-
-                {/* CIA World Factbook */}
-
-                <ProductSectionTitle icon={<LawIcon />} label='Crime & Justice' />
-
-                <HStack justify='around'>
-                    <ProductResult
-                        icon={mostWantedLogo}
-                        label='FBI Most Wanted'
-                        url='most-wanted'
-                    />
-                </HStack>
-
-                <Spacer />
-            </VStack>
-        </HStack>
-    );
-}
+import LandingPage from './LandingPage';
 
 function JustStocks() {
     return (
---
+++ app/src/LandingPage.tsx
@@ -0,0 +1,54 @@
+import { GraphIcon, ArchiveIcon, LawIcon } from '@primer/octicons-react';
+import { HStack, VStack, Spacer } from 'reaction';
+import { ProductSectionTitle, ProductResult } from './components/ProductComponents';
+import SearchBar from './components/SearchBar';
+import './LandingPage.css';
+import logo from './logo.png';
+import stocksLogo from './stocks.png';
+import mostWantedLogo from './Just Most Wanted.png';
+
+function LandingPage() {
+    return (
+        <HStack height='100%'>
+            <VStack className='landing-left'>
+                <Spacer />
+                <img src={logo} alt='Just Data' className='landing-brand' />
+                <Spacer />
+                <span className='bg-attribution' style={{ alignSelf: 'start' }}>
+                    Photo by Maximalfocus on Unsplash
+                </span>
+            </VStack>
+            <VStack className='landing-divider'>&nbsp;</VStack>
+            <VStack className='landing-right'>
+                <SearchBar placeholder='Search Our Products' />
+                <div style={{ height: '2rem' }} />
+
+                <ProductSectionTitle icon={<GraphIcon />} label='Financial Markets' />
+
+                <HStack justify='around'>
+                    <ProductResult icon={stocksLogo} label='Stocks' url='stocks' />
+                </HStack>
+
+                {/* Stock information */}
+
+                <ProductSectionTitle icon={<ArchiveIcon />} label='U.S. Intelligence' />
+
+                {/* CIA World Factbook */}
+
+                <ProductSectionTitle icon={<LawIcon />} label='Crime & Justice' />
+
+                <HStack justify='around'>
+                    <ProductResult
+                        icon={mostWantedLogo}
+                        label='FBI Most Wanted'
+                        url='most-wanted'
+                    />
+                </HStack>
+
+                <Spacer />
+            </VStack>
+        </HStack>
+    );
+}
+
+export default LandingPage;
