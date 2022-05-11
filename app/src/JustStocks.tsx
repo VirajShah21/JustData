1c450795ab38a82d90524cc5818f0f511b3545df
Viraj Shah <shahvirajd@gmail.com>
2022-05-10 23:24:09 -0400

    Added ability to change ticker symbol

--- app/src/JustStocks.tsx
+++ app/src/JustStocks.tsx
@@ -1,4 +1,5 @@
 import { GraphIcon } from '@primer/octicons-react';
+import { useState } from 'react';
 import { AdvancedRealTimeChart } from 'react-ts-tradingview-widgets';
 import { HStack, VStack } from 'reaction';
 import BrandButton from './components/BrandButton';
@@ -6,16 +7,24 @@ import FeatureButton from './components/FeatureButton';
 import SearchBar from './components/SearchBar';
 
 function JustStocks() {
+    const [searchInput, setSearchInput] = useState('');
+
     return (
         <VStack width='100%'>
             <HStack>
                 <BrandButton />
-                <SearchBar placeholder='Search Stocks and Ticker Symbols' />
+                <SearchBar
+                    placeholder='Search Stocks and Ticker Symbols'
+                    value={searchInput}
+                    onChange={e => {
+                        setSearchInput(e.target.value);
+                    }}
+                />
             </HStack>
             <HStack>
                 <FeatureButton icon={<GraphIcon />} label='Charts' active />
             </HStack>
-            <AdvancedRealTimeChart theme='dark' width='100%' autosize />
+            <AdvancedRealTimeChart theme='dark' width='100%' symbol={searchInput} autosize />
         </VStack>
     );
 }
--- app/src/components/SearchBar.tsx
+++ app/src/components/SearchBar.tsx
@@ -1,22 +1,44 @@
 import { SearchIcon, ArrowRightIcon } from '@primer/octicons-react';
-import { HStack } from 'reaction';
+import { HStack, VStack } from 'reaction';
 import './SearchBar.css';
 
-function SearchBar(props: { placeholder?: string; value?: string }) {
+function SearchBar(props: {
+    placeholder?: string;
+    value?: string;
+    onChange?: React.ChangeEventHandler<HTMLInputElement>;
+}) {
     return (
-        <HStack className='search-bar'>
-            <span className='search-icon-wrapper'>
-                <SearchIcon />
-            </span>
-            <input
-                type='text'
-                value={props.value}
-                placeholder={props.placeholder}
-                className='search-bar-input'
-            />
-            <button className='search-button'>
-                <ArrowRightIcon size={25} />
-            </button>
+        <div className='search-bar-container'>
+            <HStack className='search-bar'>
+                <span className='search-icon-wrapper'>
+                    <SearchIcon />
+                </span>
+                <input
+                    type='text'
+                    value={props.value}
+                    placeholder={props.placeholder}
+                    className='search-bar-input'
+                    onChange={props.onChange}
+                />
+                <button className='search-button'>
+                    <ArrowRightIcon size={25} />
+                </button>
+            </HStack>
+            <VStack className='search-bar-autocomplete' width='100%'>
+                <SearchBarSuggestion value='Hello World' />
+            </VStack>
+        </div>
+    );
+}
+
+function SearchBarSuggestion(props: { image?: string; icon?: JSX.Element; value?: string }) {
+    return (
+        <HStack className='search-bar-suggestion' justify='start'>
+            {props.image && (
+                <img className='search-bar-suggestion-image' src={props.image} alt={props.value} />
+            )}
+            {props.icon && <span className='search-bar-suggestion-icon'>{props.icon}</span>}
+            <span className='search-bar-suggestion-value'>{props.value}</span>
         </HStack>
     );
 }
