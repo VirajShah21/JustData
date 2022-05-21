f459898b7e67f77c46544b85f0d5ce56563a3db5
Viraj Shah <shahvirajd@gmail.com>
2022-05-20 23:36:33 -0400

    New: Scraper for White House financial disclosures

---
+++ server/src/Scraper/WHFinancialDisclosureScraper.ts
@@ -0,0 +1,64 @@
+import { loggers } from 'winston';
+import Logger from '../utils/Logger';
+import { sleep } from '../utils/TimeFunctions';
+import Scraper from './Scraper';
+
+type WHFinancialDisclosures = {
+    president: Record<number, string>;
+    vp: Record<number, string>;
+};
+
+class WHFinancialDisclosureScraper extends Scraper<WHFinancialDisclosures> {
+    constructor() {
+        super('https://www.whitehouse.gov/disclosures/financial-disclosures/');
+    }
+
+    async scrape(): Promise<WHFinancialDisclosures> {
+        await this.openTab();
+
+        const links = (await this.select('body'))[0].querySelectorAll('a').filter(a => {
+            const text = a.textContent.trim();
+            return (
+                (text.includes('President') || text.includes("Vice President's")) &&
+                text.includes('Financial Disclosure Report')
+            );
+        });
+
+        this.closeTab();
+
+        const presidentsReports: Record<number, string> = {};
+        const vpReports: Record<number, string> = {};
+
+        links.forEach(link => {
+            const text = link.textContent.trim();
+
+            // The presidents link
+            if (text.indexOf('President') === 0) {
+                const year = parseInt(text.split(' ')[1]);
+                const url = link.getAttribute('href') ?? '#';
+                presidentsReports[year] = url;
+            }
+
+            // The VP link
+            if (text.indexOf('Vice President') === 0) {
+                const year = parseInt(text.split(' ')[2]);
+                const url = link.getAttribute('href') ?? '#';
+                vpReports[year] = url;
+            }
+        });
+
+        // TODO: Add caching
+
+        return {
+            president: presidentsReports,
+            vp: vpReports,
+        };
+    }
+
+    // TODO: Implement caching
+    get cache(): WHFinancialDisclosures {
+        throw new Error('Not implemented yet');
+    }
+}
+
+export default WHFinancialDisclosureScraper;
