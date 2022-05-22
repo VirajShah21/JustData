8035b6f1b4a7df4c6cbb610eba529cd8aa3d67f0
Viraj Shah <shahvirajd@gmail.com>
2022-05-22 04:00:17 -0400

    Moved interface to SERP

--- shared/@types/SERP.d.ts
+++ shared/@types/SERP.d.ts
@@ -3,3 +3,24 @@ interface SERPItem {
     url: string;
     description: string;
 }
+
+interface YellowPagesSearchResult {
+    business: string;
+    categories: string[];
+    rating: YellowPagesListingRating;
+    website?: string;
+    phone: string;
+    address: {
+        street: string;
+        city: string;
+        state: string;
+        zip: string;
+    };
+    age?: number;
+    openStatus: string;
+}
+
+interface YellowPagesListingRating {
+    yellowPages?: number;
+    tripAdvisor?: number;
+}
