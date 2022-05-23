3a4771f5849a5301d81faee5cf595d819c6bcc54
Viraj Shah <shahvirajd@gmail.com>
2022-05-23 00:42:45 -0400

    Fix: Using async/await and HTTPError

--- app/src/utils/JustSDK.ts
+++ app/src/utils/JustSDK.ts
@@ -2,6 +2,13 @@ import axios from 'axios';
 
 const httpSuccess = 200;
 
+class HTTPError extends Error {
+    constructor(status: number) {
+        super(`HTTP error: ${status}`);
+        this.name = 'HTTPError';
+    }
+}
+
 class JustSDK {
     private static readonly DEV_DOMAIN = `http://localhost:3001`;
 
@@ -14,80 +21,64 @@ class JustSDK {
 
 class FBIKit {
     async requestTenMostWantedFugitives(): Promise<SimpleFugitiveData[]> {
-        return new Promise((resolve, reject) => {
-            axios.get(`${JustSDK.hostname}/api/fbi/ten-most-wanted`).then(response => {
-                if (response.status === httpSuccess) resolve(response.data);
-                else reject(response.status);
-            });
-        });
+        const response = await axios.get(`${JustSDK.hostname}/api/fbi/ten-most-wanted`);
+        if (response.status === httpSuccess) return response.data;
+        else throw new HTTPError(response.status);
     }
 
     async requestAllFugitives(): Promise<FullFugitiveData[]> {
-        return new Promise((resolve, reject) => {
-            axios.get(`${JustSDK.hostname}/api/fbi/all-fugitives`).then(response => {
-                if (response.status === httpSuccess) resolve(response.data);
-                else reject(response.status);
-            });
-        });
+        const response = await axios.get(`${JustSDK.hostname}/api/fbi/all-fugitives`);
+        if (response.status === httpSuccess) return response.data;
+        else throw new HTTPError(response.status);
     }
 }
 
 class PlacesKit {
     async search(query: string, location: string): Promise<YellowPagesSearchResult[]> {
-        return new Promise((resolve, reject) => {
-            axios
-                .get(`${JustSDK.hostname}/api/business/search?q=${query}&location=${location}`)
-                .then(response => {
-                    if (response.status === httpSuccess) resolve(response.data);
-                    else reject(response.status);
-                });
-        });
+        const response = await axios.get(
+            `${JustSDK.hostname}/api/business/search?q=${query}&location=${location}`,
+        );
+
+        if (response.status === httpSuccess) return response.data;
+        else throw new HTTPError(response.status);
     }
 }
 
 class SCOTUSKit {
     async getCaseList(...terms: string[]) {
-        return new Promise((resolve, reject) => {
-            const query =
-                terms.length === 1 ? `term=${terms[0]}` : `terms=${encodeURI(terms.toString())}`;
-            axios.get(`${JustSDK.hostname}/api/supreme-court/cases?${query}`).then(response => {
-                if (response.status === httpSuccess) {
-                    const arr = [];
-                    for (const term in response.data) {
-                        if (response.data.hasOwnProperty(term)) {
-                            arr.push(...response.data[term]);
-                        }
-                    }
-                    resolve(arr);
-                } else {
-                    reject(response.status);
+        const query =
+            terms.length === 1 ? `term=${terms[0]}` : `terms=${encodeURI(terms.toString())}`;
+        const response = await axios.get(`${JustSDK.hostname}/api/supreme-court/cases?${query}`);
+        if (response.status === httpSuccess) {
+            const arr = [];
+            for (const term in response.data) {
+                if (response.data.hasOwnProperty(term)) {
+                    arr.push(...response.data[term]);
                 }
-            });
-        });
+            }
+            return arr;
+        } else {
+            throw new HTTPError(response.status);
+        }
     }
 }
 
 class SecuritiesKit {
     async searchTickerSymbols(query: string): Promise<StockTickerScraperResponse> {
-        return new Promise((resolve, reject) => {
-            axios
-                .get(`${JustSDK.hostname}/api/stocks/ticker-search?q=${encodeURI(query)}`)
-                .then(response => {
-                    if (response.status === httpSuccess) resolve(response.data);
-                    else reject(response.status);
-                });
-        });
+        const response = await axios.get(
+            `${JustSDK.hostname}/api/stocks/ticker-search?q=${encodeURI(query)}`,
+        );
+
+        if (response.status === httpSuccess) return response.data;
+        else throw new HTTPError(response.status);
     }
 }
 
 class SERPKit {
     async bing(query: string): Promise<SERPItem[]> {
-        return new Promise((resolve, reject) => {
-            axios.get(`${JustSDK.hostname}/api/serp?q=${encodeURI(query)}`).then(response => {
-                if (response.status === httpSuccess) resolve(response.data);
-                else reject();
-            });
-        });
+        const response = await axios.get(`${JustSDK.hostname}/api/serp?q=${encodeURI(query)}`);
+        if (response.status === httpSuccess) return response.data;
+        else throw new HTTPError(response.status);
     }
 }
 
