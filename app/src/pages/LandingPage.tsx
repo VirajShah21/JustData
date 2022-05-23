760ed0b17411caef2b19ac9f96b7f92346b753cc
Viraj Shah <shahvirajd@gmail.com>
2022-05-22 20:38:48 -0400

    Fix: Landing page products are now searchable

--- app/src/pages/LandingPage.tsx
+++ app/src/pages/LandingPage.tsx
@@ -1,4 +1,5 @@
 import { ArchiveIcon, GraphIcon, LawIcon, SearchIcon } from '@primer/octicons-react';
+import React, { useState } from 'react';
 import { HStack, Spacer, VStack } from 'reaction';
 import { ProductResult, ProductSectionTitle } from '../components/ProductComponents';
 import SearchBar from '../components/SearchBar';
@@ -9,6 +10,19 @@ import justSecuritiesLogo from '../resources/images/icons/Just Securities.png';
 import logo from '../resources/images/icons/logo.png';
 import searchLogo from '../resources/images/icons/search.png';
 import './LandingPage.css';
+
+/**
+ * @returns The landing page for Just Data.
+ */
+function LandingPage() {
+    return (
+        <HStack height='100%'>
+            <LandingLeft />
+            <VStack className='landing-divider'>&nbsp;</VStack>
+            <LandingRight />
+        </HStack>
+    );
+}
 
 /**
  * @returns The left side of the landing page. This incldues a logo and a
@@ -29,6 +43,32 @@ function LandingLeft() {
     );
 }
 
+enum ProductCategoryRef {
+    FinancialMarkets,
+    USIntelligence,
+    CrimeAndJustice,
+    WebSearch,
+}
+
+const CATEGORY_REF_MAPPING = {
+    [ProductCategoryRef.FinancialMarkets]: 'Financial Markets',
+    [ProductCategoryRef.USIntelligence]: 'United States Intelligence',
+    [ProductCategoryRef.CrimeAndJustice]: 'Crime and Justice',
+    [ProductCategoryRef.WebSearch]: 'Web Search',
+};
+
+interface ProductCategory {
+    category: ProductCategoryRef;
+    icon: React.ReactElement;
+    products: Product[];
+}
+
+interface Product {
+    product: string;
+    icon: string;
+    url: string;
+}
+
 /**
  * A React component which displays the right side of the landing page.
  * This includes the search bar and a list of products, sorted by category.
@@ -36,51 +76,128 @@ function LandingLeft() {
  * @returns The right side of the landing page.
  */
 function LandingRight() {
+    const allProducts: ProductCategory[] = [
+        {
+            category: ProductCategoryRef.FinancialMarkets,
+            icon: <GraphIcon />,
+            products: [
+                {
+                    product: 'Just Securities',
+                    icon: justSecuritiesLogo,
+                    url: 'stocks',
+                },
+            ],
+        },
+        {
+            category: ProductCategoryRef.USIntelligence,
+            icon: <ArchiveIcon />,
+            products: [
+                /* CIA World Factbook */
+            ],
+        },
+        {
+            category: ProductCategoryRef.CrimeAndJustice,
+            icon: <LawIcon />,
+            products: [
+                {
+                    product: 'Just Fugitives',
+                    icon: justFugitivesLogo,
+                    url: 'most-wanted',
+                },
+                {
+                    product: 'Just SCOTUS',
+                    icon: justSCOTUSLogo,
+                    url: 'scotus',
+                },
+            ],
+        },
+        {
+            category: ProductCategoryRef.WebSearch,
+            icon: <SearchIcon />,
+            products: [
+                {
+                    product: 'Just Search',
+                    icon: searchLogo,
+                    url: 'search',
+                },
+                {
+                    product: 'Just Places',
+                    icon: justPlacesLogo,
+                    url: 'places',
+                },
+            ],
+        },
+    ].sort((a, b) => a.category - b.category);
+
+    const [products, setProducts] = useState(allProducts);
+    const [search, setSearch] = useState('');
+
     return (
-        <VStack className='landing-right'>
-            <SearchBar placeholder='Search Our Products' />
+        <VStack className='landing-right' justify='start'>
+            <SearchBar
+                value={search}
+                placeholder='Search Our Products'
+                onChange={e => {
+                    const value = e.target.value;
+                    setSearch(value);
+
+                    setProducts(
+                        allProducts
+                            .map(category => {
+                                if (
+                                    CATEGORY_REF_MAPPING[category.category]
+                                        .toLowerCase()
+                                        .includes(value.trim().toLowerCase())
+                                ) {
+                                    return category;
+                                }
+                                return {
+                                    ...category,
+                                    products: category.products.filter(product => {
+                                        return product.product
+                                            .toLowerCase()
+                                            .includes(value.trim().toLowerCase());
+                                    }),
+                                };
+                            })
+                            .filter(category => category.products.length > 0),
+                    );
+                }}
+                searchDisabled
+            />
 
             <div style={{ height: '2rem' }} />
 
-            <ProductSectionTitle icon={<GraphIcon />} label='Financial Markets' />
-
-            <HStack justify='start'>
-                <ProductResult icon={justSecuritiesLogo} label='Just Securities' url='stocks' />
-            </HStack>
-
-            <ProductSectionTitle icon={<ArchiveIcon />} label='U.S. Intelligence' />
-
-            {/* CIA World Factbook */}
-
-            <ProductSectionTitle icon={<LawIcon />} label='Crime & Justice' />
-
-            <HStack justify='start'>
-                <ProductResult icon={justFugitivesLogo} label='Just Fugitives' url='most-wanted' />
-                <ProductResult icon={justSCOTUSLogo} label='Just SCOTUS' url='/scotus' />
-            </HStack>
-
-            <ProductSectionTitle icon={<SearchIcon />} label='Web Search' />
-
-            <HStack justify='start'>
-                <ProductResult icon={searchLogo} label='Just Search' url='/search' />
-                <ProductResult icon={justPlacesLogo} label='Just Places' url='/places' />
-            </HStack>
-
-            <Spacer />
+            {products.map(category => (
+                <ProductSection
+                    key={category.category}
+                    title={CATEGORY_REF_MAPPING[category.category]}
+                    icon={category.icon}>
+                    {category.products.map(product => (
+                        <ProductResult
+                            key={product.product}
+                            icon={product.icon}
+                            label={product.product}
+                            url={product.url}
+                        />
+                    ))}
+                </ProductSection>
+            ))}
         </VStack>
     );
 }
 
-/**
- * @returns The landing page for Just Data.
- */
-function LandingPage() {
+function ProductSection(props: {
+    icon: React.ReactElement;
+    title: string;
+    children?: React.ReactNode;
+}) {
     return (
-        <HStack height='100%'>
-            <LandingLeft />
-            <VStack className='landing-divider'>&nbsp;</VStack>
-            <LandingRight />
-        </HStack>
+        <>
+            <ProductSectionTitle icon={props.icon} label={props.title} />
+
+            <HStack justify='start'>{props.children}</HStack>
+        </>
     );
 }
 
